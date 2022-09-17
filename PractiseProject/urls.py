af099675b76f0db36ca21bda207e1e78aa8f1816
Md Hafizur Rahman <44112062+Md-Hafizur-Rahman@users.noreply.github.com>
2022-09-17 23:15:57 +0600

    some change

--- PractiseProject/urls.py
+++ PractiseProject/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path ,include 
 from .views import AdminPanal
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('curd/',include('curd.urls')),
     path('relationproject/',include('relationproject.urls')),
     path('djangosignals/',include('djangosignals.urls')),
